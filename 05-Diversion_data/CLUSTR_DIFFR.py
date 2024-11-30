f1491d6e7c349e3d9328b8f91ee8c4cab4969909
JulioUri <115355138+JulioUri@users.noreply.github.com>
2024-11-30 13:27:50 +0100

    Reuploading file
    Reuploading file that calculates difference between the solutions with and without history

--- 05-Diversion_data/CLUSTR_DIFFR.py
+++ 05-Diversion_data/CLUSTR_DIFFR.py
@@ -19,9 +19,9 @@ import h5py
 
 # Decide which value of R is used:
 ## R=7/9;   R=1;   R=11/9
-R_folder = np.array(["/R=0.78", "/R=1.00", "/R=1.22"])
+R_folder = np.array(["/R=0.78", "/R=1.22"])
 
-R_v      = np.array(["0.78", "1.0", "1.22"])
+R_v      = np.array(["0.78", "1.22"])
 
 # Decide which field flow is used:
 ## 1: Double Gyre;   3: Bickley Jet;   4: Faraday Flow
@@ -32,62 +32,74 @@ St_v = np.array([0.1, 1.0, 10.0])
 
 
 
-############################################
-# Load data from files and calculate FTLEs #
-############################################
+#################################################################
+# Load data from files and calculate final position differences #
+#################################################################
 
 # Loop over field folders
 for i in range(0, len(Flowfield_folder)):
-        
+    
+    # Load initial consitions (dimensionfull data).
+    # Define length scales of the flow in case of wanting to nondimensionalise
+    # Print on console the field for which we are obtaining the data
     if Flowfield_folder[i] == "/01-Double_Gyre/":
-        # Import Initial Condition file
+        # Load initial conditions
         mat = scipy.io.loadmat('../IniCondDblGyre.mat')
-        print("      - In the Double Gyre.")
+        L_scale = 1.0
+        print("      - Double Gyre.")
         
     elif Flowfield_folder[i] == "/02-Bickley_Jet/":
-        # Import Initial Condition file
+        # Load initial conditions
         mat = scipy.io.loadmat('../IniCondBickley.mat')
+        L_scale = 1.770 
         print("      - Bickley Jet.")
         
     elif Flowfield_folder[i] == "/03-Faraday_Flow/":
-        # Import Initial Condition file
+        # Load initial conditions
         mat = scipy.io.loadmat('../IniCondFaraday.mat')
+        L_scale = 0.052487
         print("      - Faraday Flow.")
+    
     
     
     # Set dictionaries where arrays are saved to
     Aver_ndif_dic         = dict()
     Aver_adif_dic         = dict()
     Stdv_ndif_dic         = dict()
+    Stdv_adif_dic         = dict()
     max_dic               = dict()
     noHistory_count_dic   = dict()
     fullHistory_count_dic = dict()
     # Loop over R folders
     for k in range(0, len(R_folder)):
         
+        # Print on console the R for which we are obtaining the data
         if R_v[k] == "0.78":
             print("      - Checking final trajectory points for R = 0.78.\n")
             
-        elif R_v[k] == "1.0":
-            print("      - Checking final trajectory points for R = 1.00.\n")
-            
         elif R_v[k] == "1.22":
             print("      - Checking final trajectory points for R = 1.22.\n")
-            
+        
+        
+        
         # Set folder addresses where data is collected and saved
         load_input_from = "../01-Trajectory_data" + R_folder[k] + Flowfield_folder[i]
         save_output_in  = "." + Flowfield_folder[i]
         
-        # Import initial positions
-        x0     = mat['X']
-        y0     = mat['Y']
+        
+        # Import initial positions (Divide by L_scale for nondimensionalised plot)
+        x0     = mat['X'] # / L_scale
+        y0     = mat['Y'] # / L_scale
         
         N, L   = np.shape(x0)
         n      = int(N * L)
         
+        
+        
         Aver_ndif_v         = np.array([])
         Aver_adif_v         = np.array([])
         Stdv_ndif_v         = np.array([])
+        Stdv_adif_v         = np.array([])
         max_v               = np.array([])
         noHistory_count_v   = np.array([])
         fullHistory_count_v = np.array([])
@@ -97,11 +109,15 @@ for i in range(0, len(Flowfield_folder)):
             
             noHistory_count     = 0
             fullHistory_count   = 0
+            
+            
+            
             # Loop over all particles
             for elem in range(0, n):
                 
                 try:
-                    # Import Stokes data
+                    
+                    # Import Stokes data (trajectories without history)
                     with h5py.File(load_input_from + 'b02_STKS2_TRAJCT-R=' +\
                            R_v[k] + '-St=' + str(St_v[j]) + '.hdf5', "r") as f:
                     
@@ -121,6 +137,12 @@ for i in range(0, len(Flowfield_folder)):
                             if noHistory_fpos[0] < x0.min() or noHistory_fpos[0] > x0.max() or\
                                noHistory_fpos[1] < y0.min() or noHistory_fpos[1] > y0.max():
                                    noHistory_count += 1
+                    
+                    # Multiply by lengthscale since all our calculations are
+                    # dimensionless
+                    noHistory_fpos *= L_scale
+                    
+                    
                     
                     # Import Full MRE data
                     with h5py.File(load_input_from + 'b06_IMEX2_TRAJCT-R=' +\
@@ -144,6 +166,13 @@ for i in range(0, len(Flowfield_folder)):
                                fullHistory_fpos[1] < y0.min() or fullHistory_fpos[1] > y0.max():
                                    fullHistory_count += 1
                     
+                    # Multiply by lengthscale since all our calculations are
+                    # dimensionless
+                    fullHistory_inic *= L_scale
+                    fullHistory_fpos *= L_scale
+                    
+                    
+                    
                     # Set arrays where to save data
                     if elem == 0:
                         noHistory_fpos_v   = noHistory_fpos
@@ -159,6 +188,8 @@ for i in range(0, len(Flowfield_folder)):
                                                        np.linalg.norm(fullHistory_fpos - fullHistory_inic)))
                 except: pass
             
+            
+            
             # Save data to plot
             noHistory_count_v   = np.append(noHistory_count_v, noHistory_count)
             fullHistory_count_v = np.append(fullHistory_count_v, fullHistory_count)
@@ -172,35 +203,40 @@ for i in range(0, len(Flowfield_folder)):
             
             Stdv_ndif           = np.std(Diff_fpos_v / Aver_tdis)
             Stdv_ndif_v         = np.append(Stdv_ndif_v, Stdv_ndif)
+            Stdv_adif_v         = np.append(Stdv_adif_v, np.std(Diff_fpos_v))
             
             Diff_max            = np.linalg.norm(Diff_fpos_v, ord=np.inf) 
             max_v               = np.append(max_v, Diff_max)
+        
+        
         
         # Save data into dictionaries
         Aver_ndif_dic[R_v[k]]         = Aver_ndif_v
         Aver_adif_dic[R_v[k]]         = Aver_adif_v
         Stdv_ndif_dic[R_v[k]]         = Stdv_ndif_v
+        Stdv_adif_dic[R_v[k]]         = Stdv_adif_v
         max_dic[R_v[k]]               = max_v
         noHistory_count_dic[R_v[k]]   = noHistory_count_v
         fullHistory_count_dic[R_v[k]] = fullHistory_count_v
-        
+    
+    
+    
     # Start plotting process
     lw = 1.4
     fs = 6
     ms = 2.2
     cs = 2
     
-    # Plot average distances
+    ###############################
+    # Plot with average distances #
+    ###############################
     fig1 = plt.figure(1, layout='tight', figsize=(2.5, 2.5))
     plt.errorbar(St_v, Aver_ndif_dic[R_v[0]], yerr=Stdv_ndif_dic[R_v[0]],
                  solid_capstyle='projecting', capsize=cs, marker="X",
                  lw=lw, label="R=7/9", markersize=ms)
-    # plt.errorbar(St_v, Aver_ndif_dic[R_v[1]], yerr=Stdv_ndif_dic[R_v[1]],
-    #               solid_capstyle='projecting', capsize=cs, marker="X",
-    #               lw=lw, label="R=1", markersize=ms)
-    plt.errorbar(St_v, Aver_ndif_dic[R_v[2]], yerr=Stdv_ndif_dic[R_v[1]],
-                 solid_capstyle='projecting', capsize=cs, marker="X",
-                 lw=lw, label="R=11/9", markersize=ms)
+    plt.errorbar(St_v, Aver_ndif_dic[R_v[1]], yerr=Stdv_ndif_dic[R_v[1]],
+                  solid_capstyle='projecting', capsize=cs, marker="X",
+                  lw=lw, label="R=1", markersize=ms)
     plt.xscale('log')
     # plt.yscale('log')
     plt.legend(fontsize=fs)
@@ -212,20 +248,24 @@ for i in range(0, len(Flowfield_folder)):
     
     plt.tick_params(axis='both', labelsize=fs)
     
+    plt.savefig(save_output_in + 'b08_CLUSTR_DIFAV.eps',
+                format='eps', dpi=200, bbox_inches='tight')
+    
     plt.savefig(save_output_in + 'b08_CLUSTR_DIFAV.pdf',
                 format='pdf', dpi=200, bbox_inches='tight')
-        
     call(["pdfcrop",
           save_output_in + 'b08_CLUSTR_DIFAV.pdf',
           save_output_in + 'b08_CLUSTR_DIFAV.pdf'])
         
     plt.close(fig1)
     
-    # Plot maximum distance
+    
+    ###############################
+    # Plot with maximum distances #
+    ###############################
     fig2 = plt.figure(2, layout='tight', figsize=(2.5, 2.5))
     plt.plot(St_v, max_dic[R_v[0]], marker="X", lw=lw, label="R=7/9", markersize=ms)
-    # plt.plot(St_v, max_dic[R_v[1]], marker="X", lw=lw, label="R=1", markersize=ms)
-    plt.plot(St_v, max_dic[R_v[2]], marker="X", lw=lw, label="R=11/9", markersize=ms)
+    plt.plot(St_v, max_dic[R_v[1]], marker="X", lw=lw, label="R=1", markersize=ms)
     plt.xscale('log')
     # plt.yscale('log')
     plt.legend(fontsize=fs)
@@ -235,16 +275,21 @@ for i in range(0, len(Flowfield_folder)):
     
     plt.tick_params(axis='both', labelsize=fs)
     
+    plt.savefig(save_output_in + 'b08_CLUSTR_DIFIN.eps',
+                format='eps', dpi=200, bbox_inches='tight')
+    
     plt.savefig(save_output_in + 'b08_CLUSTR_DIFIN.pdf',
-                format='pdf', dpi=200, bbox_inches='tight')
-        
+            format='pdf', dpi=200, bbox_inches='tight')    
     call(["pdfcrop",
           save_output_in + 'b08_CLUSTR_DIFIN.pdf',
           save_output_in + 'b08_CLUSTR_DIFIN.pdf'])
         
     plt.close(fig2)
     
-    # Save data into txt file
+    
+    ###########################
+    # Save data into txt file #
+    ###########################
     with open(save_output_in + 'History_VS_Stokes.txt', 'w') as file:
         file.write("Average and maximum difference in final-time position" +\
                    "calculated with History and without History for the ")
@@ -264,11 +309,13 @@ for i in range(0, len(Flowfield_folder)):
                            str(Aver_ndif_dic[R_v[k]][j]) + ",\n" )
                 file.write("      - Relative Standard deviation = " + \
                            str(Stdv_ndif_dic[R_v[k]][j]) + ",\n" )
-                file.write("      - Average distance = " + \
+                file.write("      - Absolute average distance = " + \
                            str(Aver_adif_dic[R_v[k]][j]) + ",\n" )
+                file.write("      - Absolute Standard deviation = " + \
+                           str(Stdv_adif_dic[R_v[k]][j]) + ",\n" )
                 file.write("      - Maximum distance = " + \
                            str(max_dic[R_v[k]][j]) + ".\n" )
                 file.write("      - Number of particles that left the domain: \n" +\
-                           "        * With History: " + str(fullHistory_count_dic[R_v[k]][j]) + "\n" +\
-                           "        * Without History: " + str(noHistory_count_dic[R_v[k]][j]) + "\n")
+                           "        * With History: "  + str(fullHistory_count_dic[R_v[k]][j]) + " of a total of " + str(n) + " (" + str(round(100 * fullHistory_count_dic[R_v[k]][j] / n, 1 )) + ") \n" +\
+                           "        * Without History: " + str(noHistory_count_dic[R_v[k]][j]) + " of a total of " + str(n) + " (" + str(round(100 * noHistory_count_dic[R_v[k]][j]   / n, 1 )) + ") \n")
 
